f7f08049da005a921fcdb7118e7edfc315256f66
vengadoranal <geralejo95@gmail.com>
2019-01-24 20:59:27 +0100

    ajustesUsuario created
    ajustes usuario contendra las funciones para modificar los datos del usuario

---
+++ BackEnd/Usuario/ajustesUsuario.php
@@ -0,0 +1,23 @@
+<?php
+
+//All code for the modification of the user including vehicle info adding and modification
+
+//this function insert a new vehicle in the DB
+function addVehicle(){
+    
+    //check if the user sent a form
+    if(isset($_POST['send'])){
+        
+        //check if user is logged?
+        //get id from session values
+        if(isset($_SESSION['id_usuario'])){
+            
+            //check for form values
+            //sanitizing and filtering
+            
+        }
+        
+        
+        
+    }
+}
