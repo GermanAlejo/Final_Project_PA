4c1390c9029a0a764b289cf60ac2fc4b8be2f664
OscarGomezGonzalez <oscar.gomezgonzalez95@gmail.com>
2019-01-24 19:28:20 +0100

    Update foro.php

--- BackEnd/Foro/foro.php
+++ BackEnd/Foro/foro.php
@@ -1,8 +1,20 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+function consultaForo($nomForo) {
+    //obtenemos la conexion con la base de datos
+    $con = dbConnection();
+    //creamos la consulta
+    $buscarUsuario = "SELECT fo.titulo, fo.etiqueta FROM FORO fo WHERE fo.titulo = ?";
+    
+    $stmt = mysqli_stmt_init($con);
+    mysqli_stmt_prepare($stmt, $buscarUsuario);
+    //metemos la variable a la consulta
+    mysqli_stmt_bind_param($stmt, "s", $nomForo);
+    //ejecutamos la consulta
+    mysqli_stmt_execute($stmt);
+    //guardamos el resultado de la consulta
+    $resultado = mysqli_stmt_get_result($stmt);
+    $fila = mysqli_fetch_assoc($resultado);
+    //devolvemos el resultado en orden titulo, etiqueta
+    return $fila;
+}
