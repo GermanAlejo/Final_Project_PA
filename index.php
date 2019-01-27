77bddd4e47b66445cc54c3fc4c6614f410a9f8f1
irodsan <43887500+irodsan@users.noreply.github.com>
2019-01-27 15:24:14 +0100

    Update index

--- index.php
+++ index.php
@@ -22,7 +22,6 @@
         <!-- Custom styles for this template -->
         <link href="FrontEnd/css/landing-page.min.css" rel="stylesheet">
 
-<<<<<<< HEAD
         <!-- Bootstrap core JavaScript -->
         <script src="../../vendor/jquery/jquery.min.js"></script>
         <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -33,13 +32,6 @@
         </script>
 
 
-=======
-        <?php
-            session_start();
-        ?>
-        
-        
->>>>>>> master
     </head>
 
     <body>
@@ -72,7 +64,6 @@
         <header class="masthead text-white text-center">
             <div class="container">
                 <div class="row">
-<<<<<<< HEAD
                     <form action="#" method="GET">
                         <div class="col-xl-12 mx-auto">
                             <h1 class="mb-5">Planea tu próximo viaje ¿A dónde vas a ir?</h1>
@@ -91,29 +82,12 @@
                                     </div>
                                     <div class="col-12 col-md-3 mb-md-0">
                                         <button type="submit" class="btn btn-block btn-primary">Search!</button>
-=======
-                    <div class="col-xl-10 mx-auto">
-                        <h1 class="mb-5">Planea tu próximo viaje ¿A dónde vas a ir?</h1>
-                    </div>
-                    <div class="col-md-12 col-lg-8 col-xl-10 mx-auto">
-                        <form action="BackEnd/viajes/viajes.php" method="POST">
-                            <div class="form-row">
-                                <div class="col-12 col-md-3 mb-2 mb-md-0">
-                                    <input name="inicio" id="inicio" type="text" class="form-control " placeholder="From...">
-                                </div>
-                                <div class="col-12 col-md-3 mb-2 mb-md-0">
-                                    <input name="fin" id="fin" type="text" class="form-control " placeholder="To...">
-                                </div>
-                                <div class="col-12 col-md-3 mb-2 mb-md-0">
-                                    <div class="form-group">
-                                        <input type="date" name="fecha" id="date" class="form-control" placeholder="Date" min="today" value="today">
->>>>>>> master
                                     </div>
                                 </div>
                                 <br>
                                 <div class="form-row">
                                     <div class="col-12 col-md-4 mb-md-0 mx-auto">
-                                        <button type="submit" class="btn btn-block btn-primary">Sign up</button>                    
+                                        <a href="FrontEnd/Usuario/registroUsuarioForm.php" class="btn btn-block btn-primary">Sign up</a>                    
                                     </div>
                                 </div>
                             </form>
